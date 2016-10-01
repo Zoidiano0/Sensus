b6bdb85583b7add8af9b982be386f41ae746d2ca
blademoon <blademoon@yandex.ru>
2016-10-01 14:58:47 +0500

    Update sensus.h

--- sensus.h
+++ sensus.h
@@ -1,14 +1,14 @@
 // ---------------------------------- BME280 SENSOR ---------------------------------- 
-#define BME280_CTEMP			1	// Receive air temperature (in Celsius degrees)
-#define BME280_PRESS			2	// Receive the atmospheric pressure (in hectopascals)
-#define BME280_HUMMI			3	// Get the value of relative humidity (in percentage)
-#define BME280_FTEMP			4	// Receive air temperature (in Fahrenheit degrees)
+#define BME280_CTEMP			1	    // Receive air temperature (in Celsius degrees)
+#define BME280_PRESS			2	    // Receive the atmospheric pressure (in hectopascals)
+#define BME280_HUMMI			3	    // Get the value of relative humidity (in percentage)
+#define BME280_FTEMP			4	    // Receive air temperature (in Fahrenheit degrees)
 // ---------------------------------- BME280 SENSOR ----------------------------------
 
 // ---------------------------------- TSL2561 SENSOR --------------------------------- 
 #define TSL2561_INFRARED_LUX	1	// Receive data on the intensity of the infrared spectrum of light (in lumens)
 #define TSL2561_VISIBLE_LUX		2	// Receive data on the intensity of the visible spectrum of light (in lumens)
-#define TSL2561_TOTAL_LUX		3	// Receive information about the overall intensity of infrared and visible spectrum of light (in lumens)
+#define TSL2561_TOTAL_LUX		3	  // Receive information about the overall intensity of infrared and visible spectrum of light (in lumens)
 // ---------------------------------- BME280 SENSOR ----------------------------------
 
 #ifndef SENSUS_H
